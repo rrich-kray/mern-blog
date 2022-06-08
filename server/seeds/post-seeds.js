7a5cc2054dc46685fa8d92e9ac0511dea7bae1ac
rrich-kray <rrich.kray@gmail.com>
2022-06-07 20:02:00 -0400

    worked on UI

--- server/seeds/post-seeds.js
+++ server/seeds/post-seeds.js
@@ -2,22 +2,18 @@ const Post = require("../models/Post");
 
 const postData = [
   {
-    title: "This is a post",
-    content: `Fugiat adipisicing reprehenderit et laboris quis fugiat enim sunt commodo exercitation sunt deserunt est ad. Aliqua officia tempor incididunt consequat irure. Deserunt quis qui non magna excepteur irure. Mollit quis laborum cillum ipsum ad nostrud nulla velit occaecat voluptate ea consequat eiusmod. Minim enim consectetur nulla velit adipisicing minim labore anim cillum officia labore eiusmod et et. Aliqua deserunt cupidatat cillum pariatur proident commodo consectetur ea exercitation ullamco adipisicing in. Minim occaecat sint laboris non aliqua est deserunt exercitation.
-    Officia sint proident irure tempor officia enim. Consectetur ex id id ad eiusmod nulla tempor magna enim cupidatat velit incididunt ipsum quis. Adipisicing adipisicing consectetur quis veniam qui anim ad non in reprehenderit proident enim do. Ad irure proident consectetur sint nulla enim duis enim.
-    Fugiat occaecat commodo cillum deserunt. Sit eiusmod ad esse sint laboris consequat cillum. Commodo minim consectetur proident ad enim cupidatat fugiat.`,
-    image_url:
-      "https://www.teahub.io/photos/full/3-31570_3840x2160-abstract-3d-art-4k-abstract-wallpaper-4k.jpg",
+    title: "The problem with OOP",
+    content: `It's a dirty, rotten, low-down trick!`,
+    image_url: "https://wallpapercave.com/wp/wp9314317.jpg",
     user_id: 1,
     category_id: 1,
   },
   {
-    title: "This is a post",
+    title: "JavaScript in 100 Seconds",
     content: `Fugiat adipisicing reprehenderit et laboris quis fugiat enim sunt commodo exercitation sunt deserunt est ad. Aliqua officia tempor incididunt consequat irure. Deserunt quis qui non magna excepteur irure. Mollit quis laborum cillum ipsum ad nostrud nulla velit occaecat voluptate ea consequat eiusmod. Minim enim consectetur nulla velit adipisicing minim labore anim cillum officia labore eiusmod et et. Aliqua deserunt cupidatat cillum pariatur proident commodo consectetur ea exercitation ullamco adipisicing in. Minim occaecat sint laboris non aliqua est deserunt exercitation.
     Officia sint proident irure tempor officia enim. Consectetur ex id id ad eiusmod nulla tempor magna enim cupidatat velit incididunt ipsum quis. Adipisicing adipisicing consectetur quis veniam qui anim ad non in reprehenderit proident enim do. Ad irure proident consectetur sint nulla enim duis enim.
     Fugiat occaecat commodo cillum deserunt. Sit eiusmod ad esse sint laboris consequat cillum. Commodo minim consectetur proident ad enim cupidatat fugiat.`,
-    image_url:
-      "https://www.teahub.io/photos/full/3-31570_3840x2160-abstract-3d-art-4k-abstract-wallpaper-4k.jpg",
+    image_url: "https://wallpapercave.com/wp/wp9314356.jpg",
     user_id: 2,
     category_id: 2,
   },
@@ -26,8 +22,7 @@ const postData = [
     content: `Fugiat adipisicing reprehenderit et laboris quis fugiat enim sunt commodo exercitation sunt deserunt est ad. Aliqua officia tempor incididunt consequat irure. Deserunt quis qui non magna excepteur irure. Mollit quis laborum cillum ipsum ad nostrud nulla velit occaecat voluptate ea consequat eiusmod. Minim enim consectetur nulla velit adipisicing minim labore anim cillum officia labore eiusmod et et. Aliqua deserunt cupidatat cillum pariatur proident commodo consectetur ea exercitation ullamco adipisicing in. Minim occaecat sint laboris non aliqua est deserunt exercitation.
     Officia sint proident irure tempor officia enim. Consectetur ex id id ad eiusmod nulla tempor magna enim cupidatat velit incididunt ipsum quis. Adipisicing adipisicing consectetur quis veniam qui anim ad non in reprehenderit proident enim do. Ad irure proident consectetur sint nulla enim duis enim.
     Fugiat occaecat commodo cillum deserunt. Sit eiusmod ad esse sint laboris consequat cillum. Commodo minim consectetur proident ad enim cupidatat fugiat.`,
-    image_url:
-      "https://www.teahub.io/photos/full/3-31570_3840x2160-abstract-3d-art-4k-abstract-wallpaper-4k.jpg",
+    image_url: "https://wallpapercave.com/wp/wp8343427.jpg",
     user_id: 3,
     category_id: 3,
   },
@@ -36,8 +31,7 @@ const postData = [
     content: `Fugiat adipisicing reprehenderit et laboris quis fugiat enim sunt commodo exercitation sunt deserunt est ad. Aliqua officia tempor incididunt consequat irure. Deserunt quis qui non magna excepteur irure. Mollit quis laborum cillum ipsum ad nostrud nulla velit occaecat voluptate ea consequat eiusmod. Minim enim consectetur nulla velit adipisicing minim labore anim cillum officia labore eiusmod et et. Aliqua deserunt cupidatat cillum pariatur proident commodo consectetur ea exercitation ullamco adipisicing in. Minim occaecat sint laboris non aliqua est deserunt exercitation.
     Officia sint proident irure tempor officia enim. Consectetur ex id id ad eiusmod nulla tempor magna enim cupidatat velit incididunt ipsum quis. Adipisicing adipisicing consectetur quis veniam qui anim ad non in reprehenderit proident enim do. Ad irure proident consectetur sint nulla enim duis enim.
     Fugiat occaecat commodo cillum deserunt. Sit eiusmod ad esse sint laboris consequat cillum. Commodo minim consectetur proident ad enim cupidatat fugiat.`,
-    image_url:
-      "https://www.teahub.io/photos/full/3-31570_3840x2160-abstract-3d-art-4k-abstract-wallpaper-4k.jpg",
+    image_url: "https://wallpapercave.com/wp/wp9225789.jpg",
     user_id: 4,
     category_id: 3,
   },
@@ -46,8 +40,7 @@ const postData = [
     content: `Fugiat adipisicing reprehenderit et laboris quis fugiat enim sunt commodo exercitation sunt deserunt est ad. Aliqua officia tempor incididunt consequat irure. Deserunt quis qui non magna excepteur irure. Mollit quis laborum cillum ipsum ad nostrud nulla velit occaecat voluptate ea consequat eiusmod. Minim enim consectetur nulla velit adipisicing minim labore anim cillum officia labore eiusmod et et. Aliqua deserunt cupidatat cillum pariatur proident commodo consectetur ea exercitation ullamco adipisicing in. Minim occaecat sint laboris non aliqua est deserunt exercitation.
     Officia sint proident irure tempor officia enim. Consectetur ex id id ad eiusmod nulla tempor magna enim cupidatat velit incididunt ipsum quis. Adipisicing adipisicing consectetur quis veniam qui anim ad non in reprehenderit proident enim do. Ad irure proident consectetur sint nulla enim duis enim.
     Fugiat occaecat commodo cillum deserunt. Sit eiusmod ad esse sint laboris consequat cillum. Commodo minim consectetur proident ad enim cupidatat fugiat.`,
-    image_url:
-      "https://www.teahub.io/photos/full/3-31570_3840x2160-abstract-3d-art-4k-abstract-wallpaper-4k.jpg",
+    image_url: "https://wallpapercave.com/wp/wp9314521.png",
     user_id: 5,
     category_id: 4,
   },
@@ -56,8 +49,7 @@ const postData = [
     content: `Fugiat adipisicing reprehenderit et laboris quis fugiat enim sunt commodo exercitation sunt deserunt est ad. Aliqua officia tempor incididunt consequat irure. Deserunt quis qui non magna excepteur irure. Mollit quis laborum cillum ipsum ad nostrud nulla velit occaecat voluptate ea consequat eiusmod. Minim enim consectetur nulla velit adipisicing minim labore anim cillum officia labore eiusmod et et. Aliqua deserunt cupidatat cillum pariatur proident commodo consectetur ea exercitation ullamco adipisicing in. Minim occaecat sint laboris non aliqua est deserunt exercitation.
     Officia sint proident irure tempor officia enim. Consectetur ex id id ad eiusmod nulla tempor magna enim cupidatat velit incididunt ipsum quis. Adipisicing adipisicing consectetur quis veniam qui anim ad non in reprehenderit proident enim do. Ad irure proident consectetur sint nulla enim duis enim.
     Fugiat occaecat commodo cillum deserunt. Sit eiusmod ad esse sint laboris consequat cillum. Commodo minim consectetur proident ad enim cupidatat fugiat.`,
-    image_url:
-      "https://www.teahub.io/photos/full/3-31570_3840x2160-abstract-3d-art-4k-abstract-wallpaper-4k.jpg",
+    image_url: "https://wallpapercave.com/wp/wp9314536.jpg",
     user_id: 6,
     category_id: 5,
   },
@@ -66,8 +58,7 @@ const postData = [
     content: `Fugiat adipisicing reprehenderit et laboris quis fugiat enim sunt commodo exercitation sunt deserunt est ad. Aliqua officia tempor incididunt consequat irure. Deserunt quis qui non magna excepteur irure. Mollit quis laborum cillum ipsum ad nostrud nulla velit occaecat voluptate ea consequat eiusmod. Minim enim consectetur nulla velit adipisicing minim labore anim cillum officia labore eiusmod et et. Aliqua deserunt cupidatat cillum pariatur proident commodo consectetur ea exercitation ullamco adipisicing in. Minim occaecat sint laboris non aliqua est deserunt exercitation.
     Officia sint proident irure tempor officia enim. Consectetur ex id id ad eiusmod nulla tempor magna enim cupidatat velit incididunt ipsum quis. Adipisicing adipisicing consectetur quis veniam qui anim ad non in reprehenderit proident enim do. Ad irure proident consectetur sint nulla enim duis enim.
     Fugiat occaecat commodo cillum deserunt. Sit eiusmod ad esse sint laboris consequat cillum. Commodo minim consectetur proident ad enim cupidatat fugiat.`,
-    image_url:
-      "https://www.teahub.io/photos/full/3-31570_3840x2160-abstract-3d-art-4k-abstract-wallpaper-4k.jpg",
+    image_url: "https://wallpapercave.com/wp/wp5231852.jpg",
     user_id: 7,
     category_id: 1,
   },
@@ -76,8 +67,7 @@ const postData = [
     content: `Fugiat adipisicing reprehenderit et laboris quis fugiat enim sunt commodo exercitation sunt deserunt est ad. Aliqua officia tempor incididunt consequat irure. Deserunt quis qui non magna excepteur irure. Mollit quis laborum cillum ipsum ad nostrud nulla velit occaecat voluptate ea consequat eiusmod. Minim enim consectetur nulla velit adipisicing minim labore anim cillum officia labore eiusmod et et. Aliqua deserunt cupidatat cillum pariatur proident commodo consectetur ea exercitation ullamco adipisicing in. Minim occaecat sint laboris non aliqua est deserunt exercitation.
     Officia sint proident irure tempor officia enim. Consectetur ex id id ad eiusmod nulla tempor magna enim cupidatat velit incididunt ipsum quis. Adipisicing adipisicing consectetur quis veniam qui anim ad non in reprehenderit proident enim do. Ad irure proident consectetur sint nulla enim duis enim.
     Fugiat occaecat commodo cillum deserunt. Sit eiusmod ad esse sint laboris consequat cillum. Commodo minim consectetur proident ad enim cupidatat fugiat.`,
-    image_url:
-      "https://www.teahub.io/photos/full/3-31570_3840x2160-abstract-3d-art-4k-abstract-wallpaper-4k.jpg",
+    image_url: "https://wallpaperaccess.com/full/7176005.jpg",
     user_id: 8,
     category_id: 2,
   },
