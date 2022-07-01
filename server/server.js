47ece0cab985452725274a1a3af7b57d33aed4a0
rrich-kray <rrich.kray@gmail.com>
2022-07-01 18:01:06 -0400

    heroku deployment prep

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { ApolloServer } = require("apollo-server-express");
 const PORT = process.env.PORT || 3001;
 const { authMiddleware } = require("./utils/auth");
+const path = require("path");
 
 const { typeDefs, resolvers } = require("./schema");
 const sequelize = require("./config/connection");
